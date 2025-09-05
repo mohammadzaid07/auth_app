c2e7d0d8b2867be6ea1c3cefbd8a449d80a4c2e4
Mohammad Zaid <mohdzaid07091997@gmail.com>
2025-09-05 20:22:37 +0530

    PORT added in env

--- backend/app.js
+++ backend/app.js
@@ -23,5 +23,7 @@ app.use(express.json()); // Middleware to parse JSON
 
 app.use("/api/auth", authRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
